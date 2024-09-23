2c293189cc34323adc9067dcfa7af33646805902
nicka-CC <128600081+nicka-CC@users.noreply.github.com>
2024-09-23 10:50:50 +0300

    feat(widget): add header

--- src/shared/ui/Link/ui-link.tsx
+++ src/shared/ui/Link/ui-link.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import styles from "@/src/shared/ui/Link/ui-link.module.css";
 import classNames from "@/src/shared/lib/classnames/classnames";
 
-export interface LinkProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   className?: string;
+  href?:string;
 }
 
-const Link = ({ className, children, ...otherProps }: LinkProps) => {
+const Link = ({ className,href, children, ...otherProps }: LinkProps) => {
   return (
-    <div
+    <a href={href}
       className={classNames(styles.link, {}, [className || ""])}  // Добавлена пустая конфигурация для Mods
       {...otherProps}
     >
       {children}
-    </div>
+    </a>
   );
 };
 
--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,22 +1,38 @@
-import Link from "next/link";
-import { Button } from "@/src/shared/ui";
+'use client'
+import { Button, Link } from "@/src/shared/ui";
 import styles from "@/src/widgets/header/index.module.css";
 import { ChevronLeft, ChevronDown } from "lucide-react";
 import classNames from "@/src/shared/lib/classnames/classnames";
+import Image from "next/image";
+import logo from "@/public/Logo.svg"
+import {useState} from "react";
 const username = "Global_layout";
 const Header = ({ className }: { className?: string }) => {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <div className={classNames(styles.header, {}, [className || ""])}>
-      <Link href="/">
-        <Button theme="mini" className={styles.button}>
-          <ChevronLeft className={styles.chevron} />
-          Назад
-        </Button>
+      <Link className={styles.logo}>
+        <Image src={logo} alt={'logo'}/>
       </Link>
-      <Button className={styles.button_name} theme="mini">
-        {username}
-        <ChevronDown />
-      </Button>
+      <div className={styles.groupfst}>
+        <div><a className={styles.a} href={"/services"}>Наши услуги</a></div>
+        <div><a className={styles.a}  href={"/prices"}>Цены</a></div>
+        <div><a className={styles.a} href={"/reviews"}>Отзывы</a></div>
+        <div><a className={styles.a} href={"/contacts"}>Контакты</a></div>
+        <div onClick={()=> setOpen(!open)}>Ещё <ChevronDown color={"red"} size={10}/></div>
+      </div>
+      <div className={styles.modal}>
+        {open && <>
+          <a href={"/sizes-print"}><div className={styles.modalString}>Нестандартные размеры печати</div>
+          </a>
+          <a href={"/delivery"}><div className={styles.modalString}>Доставка</div></a>
+          <a href={"/explotan-cats"}><div className={styles.modalString}>Взрывные котята</div>
+          </a>
+          <a href={"/photographer"}><div className={styles.modalString}>Фотограф</div>
+          </a>
+          </>
+
+        }</div>
     </div>
   );
 };
